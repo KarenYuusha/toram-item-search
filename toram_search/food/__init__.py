a1a954aed68626b3a7758e36c3dacb3f11baab0d
Khoa <62637453+KarenYuusha@users.noreply.github.com>
2026-08-15 18:06:06 +0700

    feat: export Food search API

--- toram_search/food/__init__.py
+++ toram_search/food/__init__.py
@@ -1,11 +1,15 @@
 from .data import FoodDataError, load_food_dataset, normalize_food_text, resolve_food_stat
-from .models import FoodDataset, FoodEntry, FoodStatDefinition
+from .models import FoodDataset, FoodEntry, FoodSearchOutcome, FoodStatDefinition
+from .service import FoodSearchService, is_food_intent
 
 __all__ = [
     'FoodDataError',
     'FoodDataset',
     'FoodEntry',
+    'FoodSearchOutcome',
+    'FoodSearchService',
     'FoodStatDefinition',
+    'is_food_intent',
     'load_food_dataset',
     'normalize_food_text',
     'resolve_food_stat',
